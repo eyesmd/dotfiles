fbda0d5e9ed338bf746008f8097dd5a0ae193c8a
Darío Reyes <eran6reyes@gmail.com>
2019-06-08 17:41:58 +0000

    Display script fix for turning off displays

--- .bin/display/args-auto-configure.sh
+++ .bin/display/args-auto-configure.sh
@@ -4,10 +4,11 @@ source $HOME/.bin/display/display_functions.sh
 
 if isConnected HDMI-1
 then
-    notify-send "Dual setup detected"
-    echo "--output DP-1 --off --output HDMI-1 --mode 1600x900 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 128x900 --rotate normal --output HDMI-2 --off"
+    notify-send "External display setup"
+    echo "--output DP-1 --off --output HDMI-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output eDP-1 --off --output HDMI-2 --off"
+    #echo "--output DP-1 --off --output HDMI-1 --mode 1600x900 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 128x900 --rotate normal --output HDMI-2 --off"
 else
-    notify-send "Single setup detected"
+    notify-send "Single display setup"
     echo "--output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-2 --off"
 fi
 
--- .bin/display/smooth_xrandr.sh
+++ .bin/display/smooth_xrandr.sh
@@ -10,26 +10,42 @@ DISPLAY_ROOT=$HOME/.bin/display
 
 source $DISPLAY_ROOT/display_functions.sh
 
+# get main xrandr args via argument
+if [ "$#" != 1 ]
+then
+    echo "Wrong arguments: A single script file which sends xrandr arguments to stdout should be provided. Stopping." >&2
+    exit 1
+fi
+
+main_xrandr_args=$($1) 
+
+
+# function with 'output' brightness args to all enabled displays 
 argsChangeGlobalBrightness() {
     if [ "$#" != 1 ]
     then
-        echo "Wrong arguments: A single brightness value between 0 a 1 should be supplied. Stopping." >&2
+        echo "Wrong arguments for 'argsChangeGlobalBrightness': A single brightness value between 0 a 1 should be supplied. Stopping." >&2
         exit 1
     fi
-    
+
     brightness_xrandr_args=""
     for display in "${DISPLAYS[@]}"
     do
-        if isConnected $display 
+        if isConnected $display && ! (isDisabled $display)
         then
             brightness_xrandr_args+="--output $display --brightness $1 "
         fi
     done
 }
 
+isDisabled() {
+    [[ $main_xrandr_args == *"--output $1 --off"* ]]
+}
+
+
 # apply config and lights out
 argsChangeGlobalBrightness 0.0
-xrandr $($1) $brightness_xrandr_args  
+xrandr $main_xrandr_args $brightness_xrandr_args  
 
 # wait
 sleep 1.0
